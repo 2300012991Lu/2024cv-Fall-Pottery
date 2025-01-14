faa1d98711b373ba28aeb2a6f9b71ef250300633
2300012991Lu <167422115+2300012991Lu@users.noreply.github.com>
2025-01-14 22:29:29 +0800

    Add files via upload

--- project/utils/model.py
+++ project/utils/model.py
@@ -11,6 +11,7 @@ conditioned / unconditioned control, etc.
 '''
 import torch
 from torch import nn
+import torch.nn.functional as F
 
 class Discriminator(torch.nn.Module):
     def __init__(self, resolution=64):
@@ -66,46 +67,95 @@ class Generator(torch.nn.Module):
         # TODO
         
         super(Generator, self).__init__()
+        self.cube_len = cube_len
 
-        def enblock(in_channel, out_channel, decl : bool = True):
-            return (
-                nn.Conv3d(in_channel, out_channel, kernel_size=3, padding=1),
-                nn.BatchNorm3d(out_channel),
-                nn.LeakyReLU(0.2, inplace=True),
-                nn.Conv3d(out_channel, out_channel, kernel_size=3, padding=1, stride = 2 if decl else 1),
-                nn.BatchNorm3d(out_channel),
-                nn.LeakyReLU(0.2, inplace=True),
-            )
+        class EnBlock(torch.nn.Module):
 
-        self.encoder = nn.Sequential(
-            *enblock(1, 32), # 32 (16)
-            *enblock(32, 64), # 16 (8)
-            *enblock(64, 64), # 8 (4)
-            *enblock(64, 64, decl = (cube_len == 64)), # 4
-        )
+            def __init__(self, in_channel, out_channel, bottle_neck, group, stride):
+                super(EnBlock, self).__init__()
 
-        def deblock(in_channel, out_channel, decl : bool = True, is_last : bool = False):
-            return (
-                nn.ConvTranspose3d(in_channel, out_channel, kernel_size=3, padding=1, stride=1),
-                nn.BatchNorm3d(out_channel),
-                nn.ReLU(),
-                nn.ConvTranspose3d(out_channel, out_channel, kernel_size = 4 if decl else 3, padding=1, stride = 2 if decl else 1),
-                nn.BatchNorm3d(out_channel),
-                nn.Sigmoid() if is_last else nn.ReLU(),
-            )
+                hidden_channel = (out_channel // bottle_neck) * group
+                self.conv1 = nn.Conv3d(in_channel, hidden_channel, kernel_size=1)
+                self.bn1 = nn.BatchNorm3d(hidden_channel)
+                self.conv2 = nn.Conv3d(hidden_channel, hidden_channel, kernel_size=3, padding=1, stride=stride)
+                self.bn2 = nn.BatchNorm3d(hidden_channel)
+                self.conv3 = nn.Conv3d(hidden_channel, out_channel, kernel_size=1)
+                self.bn3 = nn.BatchNorm3d(out_channel)
 
-        self.decoder = nn.Sequential(
-            *deblock(64, 64, decl = (cube_len == 64)), # 8 (4),
-            *deblock(64, 64), # 16 (8)
-            *deblock(64, 32), # 32 (16)
-            *deblock(32, 1, is_last=True), # 64 (32)
-        )
+                if in_channel != out_channel or stride != 1:
+                    self.res = nn.Conv3d(in_channel, out_channel, kernel_size=1, stride=stride)
+                else:
+                    self.res = None
 
-    def forward_encode(self, x):
-        return self.encoder(x)
+            def forward(self, X : torch.Tensor):
+                Y = F.leaky_relu(self.bn1(self.conv1(X)), 0.2)
+                Y = F.leaky_relu(self.bn2(self.conv2(Y)), 0.2)
+                Y = self.bn3(self.conv3(Y))
+                if self.res is not None:
+                    X = self.res(X)
+                return F.leaky_relu(Y + X, 0.2)
+        
+        self.en_conv = nn.Sequential(nn.Conv3d(1, 32, 7, stride=2, padding=3), nn.BatchNorm3d(32), nn.LeakyReLU(0.1)) # 32 / 16
+        self.en_block2 = EnBlock(32, 64, 4, 4, 1) # 32 / 16 <------ en_res_block1
+        self.en_block4 = EnBlock(64, 128, 4, 4, 2) # 16 / 8
+        self.en_block5 = EnBlock(128, 128, 4, 4, 1) # 16 / 8 <----- en_res_block2
+        
+        self.en_res_block1 = nn.Sequential(EnBlock(1, 64, 4, 16, 2), nn.BatchNorm3d(64)) # 32 / 16
+        self.en_res_block2 = nn.Sequential(EnBlock(64, 128, 4, 16, 2), nn.BatchNorm3d(128)) # 16 / 8
+
+        class DeBlock(torch.nn.Module):
+
+            def __init__(self, in_channel, out_channel, bottle_neck, group, stride):
+                super(DeBlock, self).__init__()
+
+                hidden_channel = (out_channel // bottle_neck) * group
+                self.conv1 = nn.ConvTranspose3d(in_channel, hidden_channel, kernel_size=1)
+                self.bn1 = nn.BatchNorm3d(hidden_channel)
+                self.conv2 = nn.ConvTranspose3d(hidden_channel, hidden_channel, kernel_size = 3 if stride == 1 else 4, padding=1, stride = 1 if stride == 1 else 2)
+                self.bn2 = nn.BatchNorm3d(hidden_channel)
+                self.conv3 = nn.ConvTranspose3d(hidden_channel, out_channel, kernel_size=1)
+                self.bn3 = nn.BatchNorm3d(out_channel)
+
+                if in_channel != out_channel or stride != 1:
+                    self.res = nn.ConvTranspose3d(in_channel, out_channel, kernel_size = 1 if stride == 1 else 2, stride = 1 if stride == 1 else 2)
+                else:
+                    self.res = None
+
+            def forward(self, X : torch.Tensor):
+                Y = F.leaky_relu(self.bn1(self.conv1(X)), 0.2)
+                Y = F.leaky_relu(self.bn2(self.conv2(Y)), 0.2)
+                Y = self.bn3(self.conv3(Y))
+                if self.res is not None:
+                    X = self.res(X)
+                return F.leaky_relu(Y + X, 0.1)
+
+        self.de_block2 = DeBlock(128, 64, 4, 8, 2) # 16 / 8
+        self.de_block3 = DeBlock(64, 64, 4, 4, 1) # 16 / 8 <----- de_res_block1
+        self.de_block5 = DeBlock(64, 32, 4, 8, 2) # 32 / 16
+        self.de_block6 = DeBlock(32, 32, 4, 4, 1) # 32 / 16 <---- de_res_block2
+        self.de_block8 = DeBlock(32, 16, 4, 8, 2) # 64 / 32
+        self.de_block9 = DeBlock(16, 16, 2, 4, 1) # 64 / 32 <---- de_res_block3
+        self.de_block10 = DeBlock(16, 16, 2, 8, 2) # 128 / 64
+        self.de_block11 = DeBlock(16, 1, 1, 8, 1) # 128 / 64 <--- de_res_block4
+        
+        self.de_res_block1 = DeBlock(128, 64, 4, 16, 2) # 16 / 8
+        self.de_res_block2 = DeBlock(64, 32, 4, 16, 2) # 32 / 16
+        self.de_res_block3 = DeBlock(32, 16, 4, 16, 2) # 64 / 32
+        self.de_res_block4 = DeBlock(16, 1, 1, 16, 2) # 128 / 64
+
+    def forward_encode(self, X : torch.Tensor):
+        Y = self.en_block2(self.en_conv(X)) + self.en_res_block1(X)
+        Y = self.en_block5(self.en_block4(Y)) + self.en_res_block2(Y)
+        Y = F.avg_pool3d(Y, 2)
+        return Y # B * 128 * (8/4)^3
     
-    def forward_decode(self, x):
-        return self.decoder(x)
+    def forward_decode(self, X : torch.Tensor):
+        Y = self.de_block3(self.de_block2(X)) + self.de_res_block1(X)
+        Y = self.de_block6(self.de_block5(Y)) + self.de_res_block2(Y)
+        Y = self.de_block9(self.de_block8(Y)) + self.de_res_block3(Y)
+        Y = self.de_block11(self.de_block10(Y)) + self.de_res_block4(Y)
+        Y = F.avg_pool3d(Y, 2) # B * 1 * (64/32)^3
+        return F.sigmoid(Y)
     
     def forward(self, x):
         # you may also find torch.view() useful
